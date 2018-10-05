278150ad617429cbf1833b5e0abe1310766325d2
steven varga <steven@vargaconsulting.ca>
2018-10-05 15:38:08 -0400

    org files by gerd

---
+++ org/compound.h
@@ -0,0 +1,15 @@
+#ifndef COMPOUND_H
+#define COMPOUND_H
+
+typedef struct s1_t {
+	int    a_name;
+	float  b_name;
+	double c_name;
+} s1_t;
+
+typedef struct s2_t {
+	double c_name;
+	int    a_name;
+} s2_t;
+
+#endif
