d1d1d9d5363564b8841043f3dd21e4842e681eb8
steven varga <steven@vargaconsulting.ca>
2019-01-23 23:28:08 -0500

    high throughput pipeline: experimental

--- h5cpp/H5Zpipeline.hpp
+++ h5cpp/H5Zpipeline.hpp
@@ -47,7 +47,7 @@ namespace h5{ namespace impl {
 		filter::call_t filter[H5CPP_MAX_FILTER];
 		hsize_t n,
 				C[H5CPP_MAX_RANK], D[H5CPP_MAX_RANK],
-				N[H5CPP_MAX_RANK], B[H5CPP_MAX_RANK], R[H5CPP_MAX_RANK];
+				N[H5CPP_MAX_RANK], B[H5CPP_MAX_RANK], Rx[H5CPP_MAX_RANK],Ry[H5CPP_MAX_RANK];
 		unsigned cd_values[H5CPP_MAX_FILTER][H5CPP_MAX_FILTER_PARAM],
 				flags[H5CPP_MAX_FILTER];
 		size_t 	block_size, element_size, cd_size[H5CPP_MAX_FILTER];
@@ -136,7 +136,7 @@ inline void h5::impl::pipeline_t<Derived>::set_cache( const h5::dcpl_t& dcpl, si
 
 #define h5cpp_outer( idx ) for( j##idx =0; j##idx < n##idx; j##idx += b##idx)
 #define h5cpp_inner( idx ) for( i##idx = j##idx; i##idx < std::min(j##idx+b##idx,n##idx); i##idx++)
-#define h5cpp_def( idx ) hsize_t i##idx=0, j##idx = 0, s##idx=0, n##idx=N[idx], b##idx=B[idx], r##idx=R[idx];
+#define h5cpp_def( idx ) hsize_t i##idx=0, j##idx = 0, s##idx=0, n##idx=N[idx], b##idx=B[idx], rx##idx=Rx[idx],  ry##idx=Ry[idx];
 
 template< class Derived>
 inline void h5::impl::pipeline_t<Derived>::split_to_chunk_read(
@@ -148,11 +148,12 @@ inline void h5::impl::pipeline_t<Derived>::split_to_chunk_read(
 	// if there are residuals on the edges, then padding is needed.
 	unsigned i=0;
 	for(; i<rank; i++ )
-		N[i] = dims[rank-i-1], R[i] = N[i] % B[i];
-	for(; i<H5CPP_MAX_RANK; i++) N[i] = B[i] = 1, R[i] = 0;
+		N[i] = dims[rank-i-1], Ry[i] = N[i] % B[i], Rx[i] = B[i] - chunk_offset[i] % B[i];
+	// set the rest to default
+	for(; i<H5CPP_MAX_RANK; i++) N[i] = B[i] = 1, Rx[i] = Ry[i] = 0;
 
-	// b - block size, j - block index pos, r - remainder at the edges, n - actual dimension of data
-	//
+	// b - block size, j - block index pos, n - actual dimension of data
+	// rx - remainder at the leading edges, ry - remainder at trailing edges 
 	h5cpp_def(0) h5cpp_def(1) h5cpp_def(2) h5cpp_def(3) h5cpp_def(4) h5cpp_def(5) h5cpp_def(6)
 
 	h5cpp_outer( 6 ){ h5cpp_outer( 5 ){ h5cpp_outer( 4 ){ h5cpp_outer( 3 ){
@@ -165,17 +166,15 @@ inline void h5::impl::pipeline_t<Derived>::split_to_chunk_read(
 		for(int k=0;k<rank; k++ ) C[k] = D[rank-k-1] + chunk_offset[k];
 		// execute filters in reverse direction
 		read_chunk( this->C, this->block_size, this->chunk0 );
-		// reset memory page only on the edges
-	//	if( j0 + b0 > n0 || j1 + b1 > n1 || j2 + b2 > n2 ||
-	//		j3 + b3 > n3 || j4 + b4 > n4 || j5 + b5 > n5 || j6 + b6 > n6 ) memset(p,0, block_size);
+
 		h5cpp_inner(6){	h5cpp_inner(5){ h5cpp_inner(4){
 		h5cpp_inner(3){ h5cpp_inner(2){ h5cpp_inner(1){
 			hsize_t offset = i6*n5*n4*n3*n2*n1*n0 + i5*n4*n3*n2*n1*n0 +
 					i4*n3*n2*n1*n0 + i3*n2*n1*n0 + i2*n1*n0 + i1*n0 + j0;
-			if( j0 != n0 - r0 ) // block copy
+			if( j0 != n0 - ry0 ) // block copy
 				memcpy(ptr + offset * element_size, p, b0 * element_size );
 			else // edge handling
-				memcpy(ptr + offset * element_size,p, r0 * element_size );
+				memcpy(ptr + offset * element_size,p, ry0 * element_size );
 			p += b0 * element_size;
 		}}}}}}
 	}}}}}}}
@@ -184,18 +183,18 @@ inline void h5::impl::pipeline_t<Derived>::split_to_chunk_read(
 template< class Derived>
 inline void h5::impl::pipeline_t<Derived>::split_to_chunk_write(
 	filter_direction_t direction, const hsize_t* chunk_offset, const hsize_t* dims, const void* ptr_ ){
-	const char* ptr = static_cast<const char*>( ptr_ );
+	const char* ptr = static_cast<const char*>( ptr_ ); const hsize_t* O = chunk_offset;
 	// compute edges if any - for the data size
 	// computes R residuals, and sets N dimension in reverse order
 	// when actual data dimensions - current_dims are greater then a chunk, it must be broken into chunk size
 	// if there are residuals on the edges, then padding is needed.
 	unsigned i=0;
 	for(; i<rank; i++ )
-		N[i] = dims[rank-i-1], R[i] = N[i] % B[i];
-	for(; i<H5CPP_MAX_RANK; i++) N[i] = B[i] = 1, R[i] = 0;
+		N[i] = dims[rank-i-1], Rx[i] = B[i] - O[i] % B[i], Ry[i] = (N[i] - Rx[i]) % B[i];
+	for(; i<H5CPP_MAX_RANK; i++) N[i] = B[i] = 1, Rx[i] = Ry[i] = 0;
 
-	// b - block size, j - block index pos, r - remainder at the edges, n - actual dimension of data
-	//
+	// b - block size, j - block index pos, n - actual dimension of data
+	// rx - remainder at the leading edges, ry - remainder at trailing edges 
 	h5cpp_def(0) h5cpp_def(1) h5cpp_def(2) h5cpp_def(3) h5cpp_def(4) h5cpp_def(5) h5cpp_def(6)
 
 	h5cpp_outer( 6 ){ h5cpp_outer( 5 ){ h5cpp_outer( 4 ){ h5cpp_outer( 3 ){
@@ -203,16 +202,16 @@ inline void h5::impl::pipeline_t<Derived>::split_to_chunk_write(
 		char* p = chunk0;
 		// reset memory page only on the edges
 		if( j0 + b0 > n0 || j1 + b1 > n1 || j2 + b2 > n2 ||
-			j3 + b3 > n3 || j4 + b4 > n4 || j5 + b5 > n5 || j6 + b6 > n6 ) memset(p,0, block_size);
+			j3 + b3 > n3 || j4 + b4 > n4 || j5 + b5 > n5 || j6 + b6 > n6 ) memset(p,0x00, block_size);
 
 		h5cpp_inner(6){	h5cpp_inner(5){ h5cpp_inner(4){
 		h5cpp_inner(3){ h5cpp_inner(2){ h5cpp_inner(1){
 			hsize_t offset = i6*n5*n4*n3*n2*n1*n0 + i5*n4*n3*n2*n1*n0 +
 					i4*n3*n2*n1*n0 + i3*n2*n1*n0 + i2*n1*n0 + i1*n0 + j0;
-			if( j0 != n0 - r0 ) // block copy
+			if( j0 != n0 - ry0 ) // block copy
 				memcpy(p, ptr + offset * element_size, b0 * element_size );
-			else // edge handling
-				memcpy(p, ptr + offset * element_size, r0 * element_size );
+			else // trailing edge handling
+				memcpy(p, ptr + offset * element_size, ry0 * element_size );
 			p += b0 * element_size;
 		}}}}}}
 		// at this point we have a single 'chunk' in this->chunk0 buffer ready to go
@@ -225,8 +224,6 @@ inline void h5::impl::pipeline_t<Derived>::split_to_chunk_write(
 		write_chunk( this->C, this->block_size, this->chunk0 );
 	}}}}}}}
 }
-
-
 #undef h5cpp_def
 #undef h5cpp_inner
 #undef h5cpp_outer
--- h5cpp/H5Zpipeline_basic.hpp
+++ h5cpp/H5Zpipeline_basic.hpp
@@ -33,24 +33,29 @@ inline void h5::impl::basic_pipeline_t::read_chunk_impl( const hsize_t* offset, 
 	size_t length = nbytes; // filter may changed this, think of compression
 	void *in = chunk0, *out=chunk1, *tmp = chunk1; // invariant: out must point to data block written
 	uint32_t filter_mask;
-	H5Dread_chunk(ds, dxpl, offset, &filter_mask, in);
 //	for(int i=0; i<9; i++)
 //		std::cout << ((short*) in)[i] << " ";
 //	std::cout<<"\n";
-/*
+
 	switch( tail ){ // tail = index pointing to queue holding filters
 		case 0: // no filters, ( if blocking ) -> data == chunk0 otherwise directly from container 
+			H5Dread_chunk(ds, dxpl, offset, &filter_mask, chunk0);
 			break;
 		case 1: // single filter
-			length = filter[0](out, in, nbytes, flags[0], cd_size[0], cd_values[0] );
+			H5Dread_chunk(ds, dxpl, offset, &filter_mask, chunk1);
+			length = filter[0](chunk0, chunk1, nbytes, flags[0], cd_size[0], cd_values[0] );
 			break;
 		default: // more than one filter
+			throw std::runtime_error("filters not implemented yet...");
+			if( tail % 2 ){
+			H5Dread_chunk(ds, dxpl, offset, &filter_mask, chunk0);
 			for(int j=tail; j>0; j--){ // invariant: out == buffer holding final result
 				tmp = in, in = out, out = tmp;
 				length = filter[j](out,in,length, flags[j], cd_size[j], cd_values[j]);
 			}
+			}
 			// direct write available from > 1.10.4
-	}*/
+	}
 }
 
 #endif
